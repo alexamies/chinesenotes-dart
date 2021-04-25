2903b11bea5cde4f23b2ae8f879c3edb265a4060
Alex Amies <alexamies@gmail.com>
2021-04-25 10:36:18 -0700

    Fix bug for source identity, clean up .gitignore

--- chrome-ext/popup.js
+++
@@ -1,6 +0,0 @@
-const multiLookupSubmit = document.getElementById("multiLookupSubmit");
-const multiLookupInput = document.getElementById("multiLookupInput");
-
-multiLookupSubmit.onclick = function() {
-  console.log(`multiLookupSubmit clicked: ${multiLookupInput.value}`);
-}
