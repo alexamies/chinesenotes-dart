d2e8b27fd9248ea336ed20249f436c85846992bd
Alex Amies <alexamies@gmail.com>
2026-05-02 17:11:28 -0700

    Update

--- bin/make_cnotes_plugin.sh
+++ bin/make_cnotes_plugin.sh
@@ -1,6 +1,6 @@
-#! /usr/bin/bash
+#!/bin/bash
 echo 'Making the Chinese Notes Chrome Plugin'
-VERSION=0.0.5
+VERSION=0.0.6
 DART_HOME=$HOME/development/flutter/bin/cache/dart-sdk
 CNOTES_READER_HOME=../chinesenotes.com
 CNOTES_DART_HOME=$PWD
@@ -8,7 +8,7 @@ EXT_HOME=chrome-ext
 cd $CNOTES_READER_HOME
 bin/make_downloads.sh
 cd $CNOTES_DART_HOME
-cp $CNOTES_READER_HOME/downloads/chinesenotes_words.json $EXT_HOME/.
+cp $CNOTES_READER_HOME/downloads/cnotes_zh_en_dict.json $EXT_HOME/chinesenotes_words.json
 cp $CNOTES_READER_HOME/downloads/modern_named_entities.json $EXT_HOME/.
 $DART_HOME/bin/dart2js --csp -o $EXT_HOME/main.dart.js web/main.dart
 $DART_HOME/bin/dart2js --csp -o $EXT_HOME/serviceworker_ext.dart.js web/serviceworker_ext.dart
